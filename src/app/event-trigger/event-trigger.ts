ee11aac08be31b9f8f8c75e1ac019dc1befa3cff
Atikul <atikuldpi@gmail.com>
2026-08-31 15:51:42 +0600

    AddSignal

--- src/app/event-trigger/event-trigger.ts
+++ src/app/event-trigger/event-trigger.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal , effect} from '@angular/core';
 
 @Component({
   selector: 'app-event-trigger',
@@ -30,16 +30,47 @@ export class EventTrigger {
   }
 
   changeColor = false
-
    toggleColor(){
     this.changeColor = !this.changeColor;
    }
 
 
    color = 1
-
     changeColorBtn(){
       if(this.color >=3) this.color = 1;
       else this.color++;
     }
+
+  users = [
+    {name: "Atikul Islam", age: 25},
+    {name: "John Doe", age: 30},
+    {name: "Jane Smith", age: 28},
+    {name: "Michael Johnson", age: 35},
+    {name: "Emily Davis", age: 22}
+  ]
+   
+  getStudentName(name:string){
+    alert("Student name: "+name);
+  }
+
+  count=signal(10);
+  x = 5;
+  
+  constructor(){
+    effect(()=>{
+      alert("count value: "+this.count());  
+    })
+  }
+  updateValue(opt:string){
+    if(opt=="inc"){
+        this.count.set(this.count() + 1);
+        this.x = this.x + 1;
+    }
+    else{
+        this.count.set(this.count() - 1);
+        this.x = this.x - 1;
+    }
+  }
+
+
 }
