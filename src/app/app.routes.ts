e7dd1d93df2c5567234b804f397ad0700685c6dc
Atikul <atikuldpi@gmail.com>
2026-09-02 15:37:11 +0600

    AddRouting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about', component: AboutComponent},
+    {path:'login', component: LoginComponent},
+    {path:'contact', component:ContactComponent},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { IncDecCounter } from './inc-dec-counter/inc-dec-counter';
 import { EventTrigger } from './event-trigger/event-trigger';
  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventTrigger, IncDecCounter],
+  imports: [RouterLink,RouterOutlet, EventTrigger, IncDecCounter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
