c8605b18122f39fc7c7a6bb9be05b6f10397435a
Atikul <atikuldpi@gmail.com>
2026-09-05 11:14:12 +0600

    Routing Practice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about';
+import { LoginComponent } from './login/login';
+import { ContactComponent } from './contact/contact';
+import { PageNotFoundComponent } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
-    {path:'about', component: AboutComponent},
+    {path:'about', component:AboutComponent},
     {path:'login', component: LoginComponent},
     {path:'contact', component:ContactComponent},
+    {path:'**',component:PageNotFoundComponent}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { IncDecCounter } from './inc-dec-counter/inc-dec-counter';
 import { EventTrigger } from './event-trigger/event-trigger';
+import { HeaderComponent } from './header/header';
+import { RouterLink, RouterOutlet } from '@angular/router';
  
 @Component({
   selector: 'app-root',
-  imports: [RouterLink,RouterOutlet, EventTrigger, IncDecCounter],
+  imports: [RouterLink,RouterOutlet, EventTrigger, IncDecCounter, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
